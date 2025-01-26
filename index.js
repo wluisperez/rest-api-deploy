a0d510584a88958f273ee215b3753793ed16df31
Luis Pérez <lperezpinatto391@gmail.com>
2025-01-26 18:55:07 -0300

    minor fix

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { corsMiddleware } from "./middlewares/cors.js";
 const app = express();
 
 app.use(json());
-app.use("/songs", songsRouter);
-app.use(corsMiddleware);
+app.use("/", songsRouter);
+app.use(corsMiddleware());
 
 const PORT = process.env.PORT ?? 1234;
 
